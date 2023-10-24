2023776bab8ae2a62c1ffa5a5dd271c10f609fda
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2023-10-24 16:56:54 +0300

    feat: added statuses for computer's components

---
+++ src/Lab2/Entities/ResultStatus.cs
@@ -0,0 +1,14 @@
+namespace Itmo.ObjectOrientedProgramming.Lab2;
+
+public class ResultStatus
+{
+    public string StatusForMotherboard { get; set; } = "Everything is great. Your computer available!";
+    public string StatusForBIOS { get; set; } = "Everything is great. Your computer available!";
+    public string StatusForCPU { get; set; } = "Everything is great. Your computer available!";
+    public string StatusForProcessorCoolingSystem { get; set; } = "Everything is great. Your computer available!";
+    public string StatusForRAM { get; set; } = "Everything is great. Your computer available!";
+    public string StatusForCaseOfComputer { get; set; } = "Everything is great. Your computer available!";
+    public string StatusForPowerUnit { get; set; } = "Everything is great. Your computer available!";
+    public string StatusOfComputer { get; set; } = "Everything is great. Your computer available!";
+    public bool HasProblems { get; set; }
+}
