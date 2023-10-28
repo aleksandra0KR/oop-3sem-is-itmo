d10f80a522c2141c6daa2685416f2561e75f19a1
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2023-10-28 14:27:40 +0300

    fix: deleted extra builders

--- src/Lab2/Services/Builder.cs
+++ src/Lab2/Services/Builder.cs
@@ -1,13 +1,26 @@
+using System.Linq;
+
 namespace Itmo.ObjectOrientedProgramming.Lab2;
 
-public abstract class Builder
+public class Builder
 {
-    public Computer Computer { get; } = new();
-    public ResultStatus Res { get; private set; } = new();
-
+    /*public ResultStatus Make_computer(Motherboard? motherboard, BIOS? bios, CPU? cpu, ProcessorCoolingSystem? processorCoolingSystem, RandomAccessMemory? randomAccessMemory, CaseOf_computer? caseOf_computer, PowerUnit? powerUnit, SSD? ssd, Hdd? hdd)
+    {
+        SetMotherboard(motherboard);
+        SetBIOS(bios);
+        SetCpu(cpu);
+        SetCoolingSystem(processorCoolingSystem);
+        SetRAM(randomAccessMemory);
+        SetCase(caseOf_computer);
+        SetPowerUnit(powerUnit);
+        SetDrive(ssd, hdd);
+        return Res;
+    }*/
+    public ResultStatus Res { get; set; } = new ResultStatus();
+    private Computer Computer { get; } = new Computer();
     public Builder CloneComputer()
     {
-        Builder builder = new BuilderWithoutSpecialElements();
+        var builder = new Builder();
         builder.Res = Res;
         builder.Computer.Motherboard = Computer.Motherboard;
         builder.Computer.Bios = Computer.Bios;
@@ -19,14 +32,143 @@ public abstract class Builder
         return builder;
     }
 
-    public abstract void SetMotherboard(Motherboard? motherboard);
-    public abstract void SetBIOS(BIOS? bios);
-    public abstract void SetCpu(CPU? cpu);
-    public abstract void SetCoolingSystem(ProcessorCoolingSystem? processorCoolingSystem);
-    public abstract void SetRAM(RandomAccessMemory? randomAccessMemory);
-    public abstract void SetCase(CaseOfComputer? caseOfComputer);
-    public abstract void SetDrive(SSD? ssd, Hdd? hdd);
-    public abstract void SetPowerUnit(PowerUnit? powerUnit);
-    public abstract void SetVideoCard(VideoCard? videoCard);
-    public abstract void SetWiFi(WiFI? wifi);
+    public void SetMotherboard(Motherboard? motherboard)
+    {
+        if (motherboard is null) throw new ValueException("Empty motherboard");
+        Computer.Motherboard = motherboard;
+    }
+
+    public void SetBIOS(BIOS? bios)
+    {
+        if (bios is null) throw new ValueException("Empty bios");
+        if (Computer.Motherboard?.TypeOfBIOS != bios.TypeOfBios)
+        {
+            Res.StatusForBIOS = "Not the needed type of Bios for motherboard, disclaimer of warranty";
+            Res.HasProblems = true;
+        }
+
+        Computer.Bios = bios;
+    }
+
+    public void SetCpu(Cpu? cpu)
+    {
+        if (cpu is null) throw new ValueException("Empty cpu");
+        if (Computer.Motherboard?.Socket != cpu.Socket)
+        {
+            Res.StatusForCPU = "Not the needed type of Bios for motherboard, disclaimer of warranty";
+            Res.HasProblems = true;
+        }
+
+        if (Computer.Bios?.NeededCPU is not null)
+        {
+            if (Computer.Bios.NeededCPU.Any(neededCpu => neededCpu == cpu.TypeOfCPU))
+            {
+                Computer.Cpu = cpu;
+                return;
+            }
+        }
+
+        Computer.Cpu = cpu;
+        Res.StatusForCPU = "Not the needed type of CPU for BIOS, disclaimer of warranty";
+        Res.HasProblems = true;
+    }
+
+    public void SetCoolingSystem(ProcessorCoolingSystem? processorCoolingSystem)
+    {
+        if (processorCoolingSystem is null) throw new ValueException("Empty Cooling System");
+        if (Computer.Cpu?.TDP <= processorCoolingSystem.TDP)
+        {
+            Computer.CoolingSystem = processorCoolingSystem;
+            return;
+        }
+
+        if (Computer.Cpu?.TDP <= 2 * processorCoolingSystem.TDP)
+        {
+            Computer.CoolingSystem = processorCoolingSystem;
+            Res.StatusForProcessorCoolingSystem = "Cooling System is too week for your CPU TDP, but will work for a while";
+            Res.HasProblems = true;
+            return;
+        }
+
+        Res.HasProblems = true;
+        Res.StatusForProcessorCoolingSystem = "Cooling System is too week for your CPU TDP, disclaimer of warranty";
+    }
+
+    public void SetRAM(RandomAccessMemory? randomAccessMemory)
+    {
+        if (randomAccessMemory is null) throw new ValueException("Empty RAM");
+        if (randomAccessMemory.Frequency < Computer.Cpu?.MaxSupportedMemoryFrequencies)
+        {
+            Res.StatusForRAM = "RAM frequency not enough for CPU, disclaimer of warranty";
+            Res.HasProblems = true;
+        }
+
+        Computer.RAM = randomAccessMemory;
+    }
+
+    public void SetCase(CaseOfComputer? caseOfComputer)
+    {
+        if (caseOfComputer is null) throw new ValueException("Empty Case");
+        if (Computer.Motherboard?.FormFactor is not null)
+        {
+            if (caseOfComputer.DimensionOfMotherBoard.Any(neededForm => neededForm == Computer.Motherboard.FormFactor))
+            {
+                Computer.CaseOfComputer = caseOfComputer;
+                return;
+            }
+        }
+
+        Res.HasProblems = true;
+        Computer.CaseOfComputer = caseOfComputer;
+        Res.StatusForCaseOfComputer = "Not the needed type of Form Factor for Mother board, disclaimer of warranty";
+    }
+
+    public void SetDrive(SSD? ssd, Hdd? hdd)
+    {
+        if (ssd is null && hdd is null) throw new ValueException("Empty HDD and SSD");
+        Computer.Hdd = hdd;
+        Computer.Ssd = ssd;
+    }
+
+    public void SetPowerUnit(PowerUnit? powerUnit)
+    {
+        if (powerUnit is null) throw new ValueException("Empty Power Unit");
+        if (2 * powerUnit.MaximumLoad < Computer.Cpu?.PowerConsumption)
+        {
+            Res.StatusForPowerUnit = "Not enough power for CPU, disclaimer of warranty";
+            Res.HasProblems = true;
+            Computer.PowerUnit = powerUnit;
+            return;
+        }
+
+        if (2 * powerUnit.MaximumLoad >= Computer.Cpu?.PowerConsumption &&
+            powerUnit.MaximumLoad < Computer.Cpu?.PowerConsumption)
+        {
+            Res.StatusForPowerUnit = "Everything is working. But Consumption little too height!";
+            Res.HasProblems = true;
+            Computer.PowerUnit = powerUnit;
+            return;
+        }
+
+        Res.StatusOfComputer = !Res.HasProblems ? "Everything is great. Your computer available!" : "Your computer has problems!";
+        Computer.PowerUnit = powerUnit;
+    }
+
+    public void SetVideoCard(VideoCard? videoCard)
+    {
+        if (Computer.Cpu != null && videoCard is null && !Computer.Cpu.EmbeddedVideoCore)
+            throw new ValueException("Empty Video Card");
+        Computer.VideoCard = videoCard;
+    }
+
+    public void SetWiFi(WiFI? wifi)
+    {
+        Computer.WiFi = wifi;
+    }
+
+    public Computer? GiveComputer()
+    {
+        if (Computer.Motherboard is not null && Computer.Cpu is not null && Computer.Bios is not null && Computer.CaseOfComputer is not null && Computer.CoolingSystem is not null && Computer.PowerUnit is not null && Computer.RAM is not null) return Computer;
+        return null;
+    }
 }
